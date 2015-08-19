5584f799ec064c26e2914a8bcf049a1c649af3a0
muneeb <11bscsmsuleman@seecs.edu.pk>
2015-08-19 21:03:51 +0500

    assignment4

---
+++ resources/views/calender.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Calender Page</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+
+</head><body>
+
+<iframe src="https://www.google.com/calendar/embed?src=11bscsmsuleman%40seecs.edu.pk&ctz=Asia/Karachi" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe></body>
+
+</html>
---
+++ resources/views/main.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home Page</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/skel.min.js"></script>
+        <script src="js/skel-layers.min.js"></script>
+        <script src="js/init.js"></script>
+        <noscript>
+            <link rel="stylesheet" href="css/skel.css" />
+
+                <link rel="stylesheet" type="text/css" href="mehran.css" />
+
+            <link rel="stylesheet" href="css/style.css" />
+            <link rel="stylesheet" href="css/style-xlarge.css" />
+        </noscript>
+<style> 
+.muneebs {
+    width: 500px
+    height: 700px
+    background-color: red;
+    position: relative;
+    -webkit-animation-name: example; /* Chrome, Safari, Opera */
+    -webkit-animation-duration: 4s; /* Chrome, Safari, Opera */
+    -webkit-animation-iteration-count: 3; /* Chrome, Safari, Opera */
+    animation-name: example;
+    animation-duration: 4s;
+    animation-iteration-count: 3;
+}
+
+/* Chrome, Safari, Opera */
+@-webkit-keyframes example {
+    0%   {background-color:red; left:0px; top:0px;}
+    25%  {background-color:yellow; left:200px; top:0px;}
+    50%  {background-color:blue; left:200px; top:200px;}
+    75%  {background-color:green; left:0px; top:200px;}
+    100% {background-color:red; left:0px; top:0px;}
+}
+
+/* Standard syntax */
+@keyframes example {
+    0%   {background-color:red; left:0px; top:0px;}
+    25%  {background-color:yellow; left:200px; top:0px;}
+    50%  {background-color:blue; left:200px; top:200px;}
+    75%  {background-color:green; left:0px; top:200px;}
+    100% {background-color:red; left:0px; top:0px;}
+}
+</style>
+    </head>
+    <body class="landing">
+
+        <!-- Header -->
+            <header id="header">
+                
+                <nav id="nav">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="me.html">clock</li>
+                        <li><a href="weather.html">weather</a></li>
+                           <li>  <a href="calender.html">Calender</a>   </li>
+                    
+                    </ul>
+                </nav>
+            </header>
+
+        <!-- Banner -->
+
+
+            <section id="banner">
+
+                <h2 class="muneeb">Dash Board</h2></div>
+                <p>A Student of Computer Science in SEECS(NUST).</p>
+<div style="margin: 15px 0px 0px; display: inline-block; text-align: center; width: 200px;"><div style="display: inline-block; padding: 2px 4px; margin: 0px 0px 5px; border: 1px solid rgb(204, 204, 204); text-align: center; background-color: rgb(255, 255, 255);"><a style="text-decoration: none; font-size: 13px; color: rgb(0, 0, 0);" href=""> </a></div><script src="http://localtimes.info/clock.php?&cp1_Hex=000000&cp2_Hex=FFFFFF&cp3_Hex=000000&fwdt=200&ham=0&hbg=0&hfg=0&sid=0&mon=0&wek=0&wkf=0&sep=0&widget_number=1000" type="text/javascript"></script></div>
+                <ul class="actions">
+                    <li>
+
+                    </li>
+                </ul>
+            </section>
+
+        <!-- One -->
+            <section id="one" class="wrapper style1 special">
+                <div class="container">
+                    <header class="major">
+                        <h2> Muneeb</h2>
+                        <p>Programmer In Computer Science Department </p>
+
+
+                    </header>
+                    <div class="row 150%">
+                        <div class="4u 12u$(medium)">
+                            <section class="box">
+                                <i class="icon big rounded color1 fa-cloud"></i>
+                                
+                                
+                            </section>
+                        </div>
+                        <div class="4u 12u$(medium)">
+                            <section class="box">
+                                <i class="icon big rounded color9 fa-desktop"></i>
+                                
+                            </section>
+                        </div>
+                        <div class="4u$ 12u$(medium)">
+                            <section class="box">
+                                <i class="icon big rounded color6 fa-rocket"></i>
+                            
+                                <p>
+
+</p>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        <!-- Two -->
+            <section id="two" class="wrapper style2 special">
+                <div class="container">
+                    <header class="major">
+                        <h2></h2>
+                        <p></p>
+                    </header>
+                                        <footer>
+                        
+                        <ul class="actions">
+                            <li>
+                            
+                            </li>
+                        </ul>
+                    </footer>
+                </div>
+            </section>
+
+        <!-- Three -->
+            
+
+        <!-- Footer -->
+            <footer id="footer">
+                                <div align="center"><h2></h2>
+            </footer>
+
+
+</script>
+
+    </body>
+</html>
